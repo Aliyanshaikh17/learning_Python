8d136b8cb19f271cb810ef0d48cc0ae3e4221f0c
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-10 21:35:37 +0530

    today i learn #declarations of string

---
+++ learning python/Strings.py
@@ -0,0 +1,17 @@
+'''
+string in python 
+'''
+
+#declarations of string 
+
+name1 = "rahul"      #double quotation
+name2 = 'shinde'     #single quotation
+
+
+#Quotes inside quotes
+sent1 = ('I want to learn "Python"')
+print(sent1)
+
+sent2 = ("I want to learn 'Python'")
+print(sent2)
+
