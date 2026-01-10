963747e05977eeb127e1e1d43a6bfd6a15d786cb
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-10 21:37:47 +0530

    find length of string

--- learning python/Strings.py
+++ learning python/Strings.py
@@ -15,3 +15,16 @@ print(sent1)
 sent2 = ("I want to learn 'Python'")
 print(sent2)
 
+
+
+# triple quotes Multiline String
+multistr = ''' i  am python ,
+Created by Guido van Rossum,
+Released in 1991 '''
+
+
+
+#Find String Length    len()
+name4 = "python"
+print(len(name4))
+
