2e43430e02dfbb7a37755e4ae3d50929a89a460f
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-11 07:00:28 +0530

    today i learn boolean data type of python

---
+++ learning python/Boolean.py
@@ -0,0 +1,22 @@
+'''
+Boolean data type
+'''
+
+#Booleans represent only two values either True or False.
+print(7 > 5 )
+print(7 < 5 )
+print(3 == 3)
+
+
+#evalute boolean value
+a = 5            #value of all integer in boolean are always true
+print("value of name a in boolean :-", bool(a))
+
+name = "chetan"
+print("value of name in boolean :-" ,bool(name))     #value of all string in boolean are always true
+
+print(bool(10))
+print(bool("Sham"))
+
+print(bool(0))
+print(bool())
