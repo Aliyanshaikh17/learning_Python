7fed987060fb7114231e2edb84f575e56b176eba
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-12 10:55:51 +0530

    today i learn Integer data type of python and Integer user input

---
+++ learning python/datatypes.py
@@ -0,0 +1,18 @@
+'''
+Data Types in Python
+(int, float, complex, boolean)
+'''
+
+
+# Integer (int)
+
+a = 10                  # positive integer
+b = -20                 # negative integer
+c = 0                   # zero value
+print(a, b, c)
+print(type(a))
+
+# user input (int)
+num = int(input("Enter an integer value:- "))
+print(num)
+
