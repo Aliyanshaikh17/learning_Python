4b1820893ed08eb3bcfbc9614fed7f9f43794268
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-14 22:51:08 +0530

    today i learn list collection in python

---
+++ learning python/list.py
@@ -0,0 +1,20 @@
+'''
+list in python 
+'''
+
+
+name = ["kunal","pranv","prashant"]                 #list []
+
+
+#use of list() constructor :-
+
+extuple = (1,2,3,4,5,6)                                 #convert extuple into list
+afterconvertion =list((extuple))
+
+
+#accessing data of the list    :-              #ex :-   print the name "umesh"  or  print 2nd value of list
+
+name = ["kunal","aman","prashant"]                     # acess data using index (name[1]) , (name[-1])
+print(name[2])
+print(name[-0])
+
