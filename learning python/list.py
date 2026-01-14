fe54f4512adc081466551fcb144c917e2263ec0d
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-14 22:52:21 +0530

    today i learn list slicing

--- learning python/list.py
+++ learning python/list.py
@@ -18,3 +18,37 @@ name = ["kunal","aman","prashant"]                     # acess data using index 
 print(name[2])
 print(name[-0])
 
+
+
+
+
+#list slicing
+b = [1,2,3,4,5,6,7]
+print(b[0:4])
+print(b[0:])
+print(b[:4])
+
+#membership oprator                           #check the data is in the list or nat
+a = [1,2,3,4,5,6,7]
+print(1 in a)
+
+
+#change the list using index
+a = [1,2,3,4,5,6,7]                         #change the list data using index funtion
+a[5] = "shaikh"
+print(a)
+
+
+#change len with range                      #change data of a specific range 
+a = [1,2,3,4,5,6,7]  
+a[:3] = "shaikh","aliyan","19"
+print(a)
+
+
+#change the data using index method                       #index method
+a = [1,2,3,4,5,6,7]
+a.insert (4,"laptop")
+print(a)
+
+
+
