fb391f3547b6dd8f895874464984f8ad108ee1d8
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-16 09:05:01 +0530

    today i learn tuples collections in python

---
+++ learning python/tuples.py
@@ -0,0 +1,18 @@
+'''
+Tuple in Python
+'''
+
+# Tuple is an ordered, immutable collection
+
+data = ("aliyan", "19", "niphad", "bsc")
+print(data)
+
+# access tuple elements
+
+print(data[0])        # access by index
+print(data[-1])       # last element
+
+print(len(data))      # for length
+print(type(data))     # for checking type
+print(tuple(data))    # convert collection to tuple
+
