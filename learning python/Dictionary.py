a0dd84217db8299b2f90fc2e4f48f791714074de
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-21 11:21:08 +0530

    today i learn what is dictionary in python

---
+++ learning python/Dictionary.py
@@ -0,0 +1,16 @@
+'''
+Dictionary in python 
+'''
+
+#Dictionary
+''' are use to store data in key value pair
+are ordered , changeble and not allow duplicate value 
+ '''
+
+data = {
+    "name":"prashant",
+    "RollNo":"19",
+    "Address":"niphad",
+    "clss":"bsc"
+}
+print(data)
