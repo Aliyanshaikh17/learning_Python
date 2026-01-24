87609575025df41ce08f91cd610ee491bf33686e
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-24 20:02:38 +0530

    today i learn Arithmetic Operators in python

---
+++ learning python/Operators.py
@@ -0,0 +1,17 @@
+'''
+Operators in python
+'''
+
+# Arithmetic Operators
+operator1 = 5
+operator2 = 10
+print(f"Addition of {operator1} and {operator2} is :- {operator1+operator2}")
+
+num1 = int(input("Enter the first number:- "))
+num2 = int(input("Enter the second number:- "))
+print("Addition operators:- ",num1 + num2)
+print("substaction operators:- ",num1 - num2)
+print("Multiplication operators:- ",num1 * num2)
+print("Division operators:- ",num1 / num2)
+print("Exponrntion operators:- ",num1 ** num2)
+print("Floor Division operators:- ",num1 // num2)
