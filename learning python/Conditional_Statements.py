86a849fdd659f047e64287d4d5bfb9aa595a3f01
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-28 11:03:09 +0530

    example of if statement

--- learning python/Conditional_Statements.py
+++ learning python/Conditional_Statements.py
@@ -5,3 +5,9 @@ Conditional Statements in python
 #if conditions  :-  
 #The if statement checks a condition.
 #If the condition is True, the code inside it runs.
+
+#ex :-
+num = 10
+
+if num > 0:
+    print("Number is positive")
