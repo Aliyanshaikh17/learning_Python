bc532ceba7aeee861964ab3c718a787ecb476434
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-04 23:02:42 +0530

    example of while loop in python

--- learning python/While_Loop.py
+++ learning python/While_Loop.py
@@ -4,3 +4,11 @@ while loop
 A while loop is a loop that keeps running until its condition becomes false.
 When the condition becomes false, the loop stops.
 '''
+
+
+#example of while loop 
+
+i = 1
+while(i <= 10):
+    print(i)
+    i = i+ 1
