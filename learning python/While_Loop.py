a8fa0e452faf93aca2ad5ff8ed0f85b7fc3c665e
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-08 10:08:11 +0530

    today i learn Nested while loop in python

--- learning python/While_Loop.py
+++ learning python/While_Loop.py
@@ -86,3 +86,15 @@ while i < 10:
         continue
     print(i)
 
+
+
+# Nested while loop
+# A while loop inside another while loop
+
+i = 1
+while i <= 3:
+    j = 1
+    while j <= 3:
+        print(i, j)
+        j = j + 1
+    i = i + 1
