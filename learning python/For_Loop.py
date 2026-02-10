2b56d5df99b0860dfba52dc4937eecda6a5125ac
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-10 22:53:02 +0530

    for loop with continue statment

--- learning python/For_Loop.py
+++ learning python/For_Loop.py
@@ -42,3 +42,13 @@ for i in range(1, 11):
     print(i)
 
 
+# for loop with continue
+# continue skips the current iteration
+
+
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
+
+
