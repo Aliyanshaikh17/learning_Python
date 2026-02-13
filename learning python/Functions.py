4fc65ccaf25cc955ac12482c5ea6872abffda427
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-14 00:05:33 +0530

    today i learn Declaring The Function.

--- learning python/Functions.py
+++ learning python/Functions.py
@@ -6,3 +6,8 @@ A function is a block of code which runs when we call that function by object.
 #How to create a function ?
 def greeting():
     print("Hello rakesha")
+
+# Declaring The Function.
+def greeting():
+    print("Hello Umesh")
+greeting()
