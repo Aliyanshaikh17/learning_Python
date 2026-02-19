e4ee90a212c7d0afad7fdfaa3879222189d821bc
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-19 08:37:48 +0530

    # Recursion Example

--- learning python/Recursion.py
+++ learning python/Recursion.py
@@ -4,3 +4,14 @@
 calls itself and stops when a base condition is satisfied. '''
 
 
+#Basic Example – Counting Numbers
+
+# Recursion Example
+
+def countDown(n):
+    if n == 0:          # Base Condition (Stopping Condition)
+        print("Done!")
+    else:
+        print(n)
+        countDown(n-1)  # Function calling itself
+countDown(5)
