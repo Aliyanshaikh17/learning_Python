0246f8b14ad9fdc750eb73a4f9ccc49e69859cc8
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-21 11:13:26 +0530

    Lambda With Arguments

--- learning python/lambda.py
+++ learning python/lambda.py
@@ -6,3 +6,14 @@ A lambda function is a small anonymous function written using the lambda keyword
 
 greeting = lambda: print("Hello")
 greeting()
+
+
+'''
+ Lambda With Arguments
+'''
+
+greeting = lambda fname: print("My name is:-", fname)
+
+greeting("Pawan")
+greeting("Rohan")
+greeting("Sham")
