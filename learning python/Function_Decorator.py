11a2b138e21f61611b5ba29b4e6c39a1c45d22b5
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-25 10:28:30 +0530

    #example of functions decorator

--- learning python/Function_Decorator.py
+++ learning python/Function_Decorator.py
@@ -8,3 +8,22 @@ Definition :-
 A decorator is a function that modifies or extends the 
 behavior of another function without changing its original code.
 '''
+
+
+
+#example of functions decorator
+
+def my_decorator(func):
+    def wrapper():
+        print("Something before function")
+        func()
+        print("Something after function")
+    return wrapper
+
+def say_hello():
+    print("Hello")
+# Applying decorator manually
+say_hello = my_decorator(say_hello)
+say_hello()
+
+
