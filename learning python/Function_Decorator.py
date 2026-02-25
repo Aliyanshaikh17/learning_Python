f9e0c84873c441de07189693c616c2dad309b3f2
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-25 10:27:38 +0530

    Functions Decorator in Python

---
+++ learning python/Function_Decorator.py
@@ -0,0 +1,10 @@
+'''
+ Functions Decorator in Python
+'''
+
+'''
+Definition :- 
+
+A decorator is a function that modifies or extends the 
+behavior of another function without changing its original code.
+'''
