21bb1dcb251a998f380579e6b7e741d7cca51edf
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-26 11:01:26 +0530

    Modules example

--- learning python/Modules.py
+++ learning python/Modules.py
@@ -6,3 +6,9 @@ Modules in python
 A module is a file that contains Python code (functions, variables, classes).
 We can reuse this code in another file using the import keyword.
 '''
+
+#example of Modules
+import math
+
+print(math.sqrt(25))
+print(math.pi)
