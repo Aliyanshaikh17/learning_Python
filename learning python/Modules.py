5036b4a9dd50c2d8a920d502a800e68ea03c12c3
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-26 10:54:48 +0530

    Modules in python

---
+++ learning python/Modules.py
@@ -0,0 +1,8 @@
+'''
+Modules in python 
+'''
+
+'''
+A module is a file that contains Python code (functions, variables, classes).
+We can reuse this code in another file using the import keyword.
+'''
