28db23a8747ba289a919ce53b6e6278157e17fb2
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-27 22:48:30 +0530

    What is Exception Handling

---
+++ learning python/Exception_Handling.py
@@ -0,0 +1,11 @@
+'''
+Exception Handling
+'''
+
+'''
+What is Exception Handling
+    - Exception Handling is used to handle errors in a program.
+    - It prevents the program from crashing.
+    - It allows the program to continue running even after an error.
+'''
+
