d0e08a73711fc69d3d31780a4db258c3fd585651
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-02-28 22:35:21 +0530

    #Example of  Handling Specific Exception

--- learning python/Exception_Handling.py
+++ learning python/Exception_Handling.py
@@ -34,3 +34,15 @@ except:
     print("Error occurred!")
 
 
+# Handling Specific Exception
+#Example
+
+try:
+    num = int(input("Enter a number:- "))
+    print(10 / num)
+
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+
+except ValueError:
+    print("Invalid input! Please enter a number.")
