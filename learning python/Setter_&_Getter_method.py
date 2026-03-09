8b3261684f7299558ce8f316313f1af459c8e7fb
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-03-09 16:37:06 +0530

    #Example of Getter Method

--- learning python/Setter_&_Getter_method.py
+++ learning python/Setter_&_Getter_method.py
@@ -9,3 +9,22 @@ Setter
     - A setter method is used to set (update) the value of a private variable.
 '''
 
+
+#Example of Getter Method
+class Student:
+
+    def __init__(self, name, marks):
+        self.__marks = marks   # private variable
+        self.name = name
+
+    # Getter Method
+    def get_marks(self):
+        return self.__marks
+
+
+s1 = Student("rohit", 85)
+
+print(s1.get_marks())
+
+
+
