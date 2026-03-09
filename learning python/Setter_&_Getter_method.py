eda95d4940a946348ef5aa0060c297a3283f02c9
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-03-09 16:35:47 +0530

    what is setter and getter method

---
+++ learning python/Setter_&_Getter_method.py
@@ -0,0 +1,11 @@
+
+'''
+Getter and Setter Method
+
+Getter
+    - A getter method is used to get (read) the value of a private variable.
+
+Setter
+    - A setter method is used to set (update) the value of a private variable.
+'''
+
