25af7d2f3c35dd9eb7d3ab17b7d4af2d96e78de9
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-03-11 10:28:56 +0530

    File Handling in Python

---
+++ learning python/File_Handling.py
@@ -0,0 +1,18 @@
+'''
+File Handling in Python
+
+    - File Handling is used to read, write, and manage files.
+    - It helps store data permanently.
+    - Python provides built-in functions for file operations.
+'''
+
+
+'''
+Steps of File Handling
+
+1) Open the file
+2) Perform operations (read / write)
+3) Close the file
+'''
+
+
